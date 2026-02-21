ddbf0463ccdf6de36dd1c9ca224baa66e63f3a85
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-02-21 18:06:06 +0100

    Initial commit
    Co-authored-by: Rosi <rosorillo@alu.edu.gva.es>

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `
+    <h1>{{ title() }}</h1>
+    <router-outlet></router-outlet>
+  `,
+})
+export class App {
+  protected readonly title = signal('rosa');
+}
