ba4a69472ecddef5a052cd012b65a0695e82283b
Mateo Manchola <mmanchola12temporal@gmail.com>
2026-02-22 04:38:05 +0100

    docs(app.routes): add TSDoc comments to routes configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
+/**
+ * Main application routes configuration.
+ *
+ * @remarks
+ * This array defines the routes for the Angular application.
+ */
 export const routes: Routes = [];
